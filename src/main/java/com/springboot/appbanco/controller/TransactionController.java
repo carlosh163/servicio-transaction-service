3d39207ea882ff9746d17a77d023cf51ddafbc53
Carlos Esteban Huaman Garay <chuamanga16>
2019-12-26 19:10:37 -0500

    version Pago desde una Cuenta

--- src/main/java/com/springboot/appbanco/controller/TransactionController.java
+++ src/main/java/com/springboot/appbanco/controller/TransactionController.java
@@ -28,306 +28,335 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
 @Api(tags = "Transactions")
 @RestController
-@RequestMapping("/api/transactions")
+//@RequestMapping("/api/transactions")
 public class TransactionController {
-  private static Logger log = LoggerFactory.getLogger(TransactionController.class);
+	private static Logger log = LoggerFactory.getLogger(TransactionController.class);
 
 	@Autowired
 	ITransactionService service;
-	
+
 	@Autowired
 	@Qualifier("cuenta")
 	WebClient wCAccount;
-	
+
 	@Autowired
 	@Qualifier("cliente")
 	WebClient wCClient;
-	
+
 	@Autowired
 	@Qualifier("persona")
 	WebClient wCPerson;
-	
+
 	@Autowired
 	@Qualifier("cuentaCredito")
 	WebClient wCAccountCredit;
-	
-	
-	@PostMapping("/deposit/{accNumber}/{quantity}/{originMov}")
-	public Mono<Transaction> deposit(@PathVariable Integer accNumber, @PathVariable double quantity,@PathVariable String originMov){
-		
-		
-		Map<String,Object> params = new HashMap();
+
+	@PostMapping("/deposit/{accNumber}/{quantity}")
+	public Mono<Transaction> deposit(@PathVariable Integer accNumber, @PathVariable double quantity) {
+
+		Map<String, Object> params = new HashMap();
 		params.put("accountNumber", accNumber);
 		params.put("quantity", quantity);
-		wCClient.put().uri("/updateBalanceAccountByAccountNumber/{accountNumber}/{quantity}",params).retrieve().bodyToFlux(Client.class).subscribe();
-		wCPerson.put().uri("/updateBalanceAccountByAccountNumber/{accountNumber}/{quantity}",params).retrieve().bodyToFlux(Person.class).subscribe();
-		return wCAccount.put().uri("/updateBalanceAccountByAccountNumber/{accountNumber}/{quantity}",params).retrieve().bodyToMono(Account.class)
-				
-					 
-				
-				.flatMap(account ->{
-					
-					Transaction objT = new Transaction();
-					
-					objT.setDate(new Date());
-					objT.setAccountNumber(accNumber);
-					objT.setQuantity(quantity);
-					objT.setTransactionType("Deposito");
-					objT.setOriginMov(originMov);
-					return service.save(objT);
+		
+		return wCClient.put().uri("/updateBalanceAccountByAccountNumber/{accountNumber}/{quantity}", params).retrieve()
+				.bodyToFlux(Client.class)
+				.next()
+				.flatMap(o ->{
+					return wCPerson.put().uri("/updateBalanceAccountByAccountNumber/{accountNumber}/{quantity}", params).retrieve()
+							.bodyToFlux(Person.class)
+							.next()
+							.flatMap(o2 ->{
+								return wCAccount.put().uri("/updateBalanceAccountByAccountNumber/{accountNumber}/{quantity}", params).retrieve()
+								.bodyToMono(Account.class)
+								.flatMap(account -> {
+
+									Transaction objT = new Transaction();
+
+									objT.setDate(new Date());
+									objT.setAccountNumber(accNumber);
+									objT.setQuantity(quantity);
+									objT.setTransactionType("Deposito");
+									objT.setOriginMov("Efectivo");
+									return service.save(objT);
+								});
+							});
 				});
+		
+		 
+
+				
 	}
-	
+
 	@PostMapping("/retirement/{accNumber}/{quantity}")
-	public Mono<Transaction> retirement(@PathVariable Integer accNumber, @PathVariable double quantity){
-		
-		
-		Map<String,Object> params = new HashMap();
+	public Mono<Transaction> retirement(@PathVariable Integer accNumber, @PathVariable double quantity) {
+
+		Map<String, Object> params = new HashMap();
 		params.put("accountNumber", accNumber);
 		params.put("quantity", quantity);
-		
+
 		Account objAcc = new Account();
 		objAcc.setAccountstatus('N');
-		
-		
-		
-		return wCAccount.put().uri("/updateBalanceAccountRetireByAccountNumber/{accountNumber}/{quantity}",params).retrieve().bodyToMono(Account.class)
-				
-				
-				.switchIfEmpty( Mono.just(objAcc))
-				
-				.flatMap(account ->{
-					
-					
-					
-					if(account.getAccountstatus() =='N') {
+
+		return wCAccount.put().uri("/updateBalanceAccountRetireByAccountNumber/{accountNumber}/{quantity}", params)
+				.retrieve().bodyToMono(Account.class)
+
+				.switchIfEmpty(Mono.just(objAcc))
+
+				.flatMap(account -> {
+
+					if (account.getAccountstatus() == 'N') {
 						return Mono.empty();
-					}else {
-						wCClient.put().uri("/updateBalanceAccountRetireByAccountNumber/{accountNumber}/{quantity}",params).retrieve().bodyToFlux(Client.class).subscribe();
-						wCPerson.put().uri("/updateBalanceAccountRetireByAccountNumber/{accountNumber}/{quantity}",params).retrieve().bodyToFlux(Person.class).subscribe();
+					} else {
+						return wCClient.put()
+								.uri("/updateBalanceAccountRetireByAccountNumber/{accountNumber}/{quantity}", params)
+								.retrieve().bodyToFlux(Client.class)
+								.next()
+								.flatMap(o ->{
+									return wCPerson.put()
+											.uri("/updateBalanceAccountRetireByAccountNumber/{accountNumber}/{quantity}", params)
+											.retrieve().bodyToFlux(Person.class)
+											.next()
+											.flatMap(o2->{
+												Transaction objT = new Transaction();
+
+												objT.setDate(new Date());
+												objT.setAccountNumber(accNumber);
+												objT.setQuantity(quantity);
+												objT.setTransactionType("Retiro");
+												objT.setOriginMov("Efectivo");
+												return service.save(objT);
+											});
+								});
 						
-						Transaction objT = new Transaction();
+
 						
-						objT.setDate(new Date());
-						objT.setAccountNumber(accNumber);
-						objT.setQuantity(quantity);
-						objT.setTransactionType("Retiro");
-						objT.setOriginMov("Efectivo");
-						return service.save(objT);
 					}
-					
-					
+
 				});
 	}
-	
-	
-	
-	
-	
+
 	@PostMapping
-	public Mono<Transaction> create(@RequestBody Transaction transt){
+	public Mono<Transaction> create(@RequestBody Transaction transt) {
 		return service.save(transt);
-		
+
 	}
-	
+
 	@GetMapping
-	public Flux<Transaction> list(){
+	public Flux<Transaction> list() {
 		return service.getAll();
 	}
-	
-	
+
 	@PostMapping("/consume/{accNumber}/{quantity}")
-	public Mono<Transaction> consume(@PathVariable Integer accNumber, @PathVariable double quantity){
-		
-		
-		Map<String,Object> params = new HashMap();
+	public Mono<Transaction> consume(@PathVariable Integer accNumber, @PathVariable double quantity) {
+
+		Map<String, Object> params = new HashMap();
 		params.put("accountNumber", accNumber);
 		params.put("quantity", quantity);
-		
+
 		CreditAccount objAcc = new CreditAccount();
 		objAcc.setAccountstatus('N');
-		
-		
-		
-		return wCAccountCredit.put().uri("/updateBalanceAccountByAccountNumberConsumer/{accountNumber}/{quantity}",params).retrieve().bodyToMono(CreditAccount.class)
-				
-				
-				.switchIfEmpty( Mono.just(objAcc))
-				
-				.flatMap(account ->{
-					
-					
-					
-					if(account.getAccountstatus() =='N') {
+
+		return wCAccountCredit.put()
+				.uri("/updateBalanceAccountByAccountNumberConsumer/{accountNumber}/{quantity}", params).retrieve()
+				.bodyToMono(CreditAccount.class)
+
+				.switchIfEmpty(Mono.just(objAcc))
+
+				.flatMap(account -> {
+
+					if (account.getAccountstatus() == 'N') {
 						return Mono.empty();
-					}else {
-						wCClient.put().uri("/updateBalanceAccountByAccountNumberConsumer/{accountNumber}/{quantity}",params).retrieve().bodyToFlux(Client.class).subscribe();
+					} else {
+						return wCClient.put()
+								.uri("/updateBalanceAccountByAccountNumberConsumer/{accountNumber}/{quantity}", params)
+								.retrieve().bodyToFlux(Client.class)
+								.next()
+								.flatMap(ob -> {
+									Transaction objT = new Transaction();
+
+									objT.setDate(new Date());
+									objT.setAccountNumber(accNumber);
+									objT.setQuantity(quantity);
+									objT.setTransactionType("Consumo");
+									objT.setOriginMov("Efectivo");
+									return service.save(objT);
+								});
+
 						
-						
-						Transaction objT = new Transaction();
-						
-						objT.setDate(new Date());
-						objT.setAccountNumber(accNumber);
-						objT.setQuantity(quantity);
-						objT.setTransactionType("Consumo");
-						objT.setOriginMov("Efectivo");
-						return service.save(objT);
 					}
-					
-					
+
 				});
-		
-		
+
 	}
+
+	@ApiOperation(value = "(P1)RQ0x-TC payment from cash", notes = "")
+	@PostMapping("/paymentCash/{accNumber}/{quantity}")
+	public Mono<Transaction> payment(@PathVariable Integer accNumber, @PathVariable double quantity) {
+
+		Map<String, Object> params = new HashMap();
+		params.put("accountNumber", accNumber);
+		params.put("quantity", quantity);
+
+		// Validando el tipo de Pago::
+			return wCAccountCredit
+					.put()
+					.uri("/updateBalanceAccounByAccountNumberPayment/{accountNumber}/{quantity}", params)
+					.retrieve()
+					.bodyToMono(CreditAccount.class).switchIfEmpty(Mono.empty()).flatMap(account -> {
+						
+							return wCClient.put()
+									.uri("/updateBalanceAccounByAccountNumberPayment/{accountNumber}/{quantity}", params)
+									.retrieve().bodyToFlux(Client.class)
+									.next()
+									.flatMap(c ->{
+										Transaction objT = new Transaction();
+
+										objT.setDate(new Date());
+										objT.setAccountNumber(accNumber);
+										objT.setQuantity(quantity);
+										objT.setTransactionType("Pago");
+										objT.setOriginMov("Efectivo");
+										return service.save(objT); 
+									});
+
+							
+						
+					});
+
+	}
+	
+	
+	
 	
 	@ApiOperation(value = "(P2)RQ06-TC payment from any Bank Account", notes = "")
-	@PostMapping("/payment/{accNumber}/{quantity}/{originMov}")
-	public Mono<Transaction> payment(@PathVariable Integer accNumber, @PathVariable double quantity,@PathVariable String originMov){
-		
-		
-		Map<String,Object> params = new HashMap();
-		params.put("accountNumber", accNumber);
+	@PostMapping("/paymentBankAccount/{accNumberCredit}/{quantity}/{numberAccBank}")
+	public Mono<Transaction> payment(@PathVariable Integer accNumberCredit, @PathVariable double quantity,@PathVariable Integer numberAccBank) {
+
+		Map<String, Object> params = new HashMap();
+		params.put("accNumberCredit", accNumberCredit);
 		params.put("quantity", quantity);
+		params.put("numberAccBank", numberAccBank);
 		
-		CreditAccount objAcc = new CreditAccount();
-		objAcc.setAccountstatus('N');
-		//Validando el tipo de Pago::
-		
-		Integer numberAccountV = null;
-		boolean typePay = true;
-		try {
-			numberAccountV=Integer.parseInt(originMov);
-			typePay= true;
-		}catch (NumberFormatException  e) {
-			typePay= false;
-		}
-		log.info("Estado del Tipo:"+typePay);
-		
-		
-		//Buscar si existe la cuenta bancaria que se desea pagar.
-		
-		
-		if(typePay == false) {
-			
-			
-			return wCAccountCredit.put().uri("/updateBalanceAccounByAccountNumberPayment/{accountNumber}/{quantity}",params).retrieve().bodyToMono(CreditAccount.class)
-					.switchIfEmpty( Mono.just(objAcc))
-					.flatMap(account ->{
-						if(account.getAccountstatus() =='N') {
+		return  wCAccount.get().uri("/findAccountByNumberAccount/{numberAccBank}", params).retrieve().bodyToMono(Account.class)
+				.switchIfEmpty(Mono.empty()).flatMap(objMAcc -> {
+					return wCAccountCredit.get().uri("/findAccountByNumberAccount/{accNumberCredit}", params).retrieve().bodyToMono(CreditAccount.class)
+					.switchIfEmpty(Mono.empty()).flatMap(objMAccCredt ->{
+						
+						
+						if ( quantity <= objMAcc.getBalance() && objMAccCredt.getConsumption()>= quantity) {
+							log.info("Procede con el pago descuento en la cuenta Corriente. -> "+ objMAcc.getBalance());
+
+							return wCAccount.put()
+									.uri("/updateBalanceAccountRetireByAccountNumber/{numberAccBank}/{quantity}",params)
+									.retrieve().bodyToMono(Account.class)
+									.flatMapMany(c ->{
+										return wCClient.put()
+												.uri("/updateBalanceAccountRetireByAccountNumber/{numberAccBank}/{quantity}",params)
+												.retrieve().bodyToFlux(Client.class);
+									})
+									.next()
+									.flatMapMany(c ->{
+										
+										return wCPerson.put()
+												.uri("/updateBalanceAccountRetireByAccountNumber/{numberAccBank}/{quantity}",params)
+												.retrieve().bodyToFlux(Person.class);
+									})
+									.next()
+									.flatMap(c->{
+										
+										return wCAccountCredit
+												.put()
+												.uri("/updateBalanceAccounByAccountNumberPayment/{accNumberCredit}/{quantity}", params)
+												.retrieve()
+												.bodyToMono(CreditAccount.class).switchIfEmpty(Mono.empty()).flatMap(account -> {
+													
+														return wCClient.put()
+																.uri("/updateBalanceAccounByAccountNumberPayment/{accNumberCredit}/{quantity}", params)
+																.retrieve().bodyToFlux(Client.class)
+																.next()
+																.flatMap(ce ->{
+																	Transaction objT = new Transaction();
+
+																	objT.setDate(new Date());
+																	objT.setAccountNumber(accNumberCredit);
+																	objT.setQuantity(quantity);
+																	objT.setTransactionType("Pago");
+																	objT.setOriginMov(String.valueOf(numberAccBank));
+																	return service.save(objT); 
+																});
+
+														
+													
+												});
+										
+										
+									});
+									
+							
+							
+
+						} else{
+							log.warn("No cuenta con saldo suficiente para la cantidad a Pagar / La cantidad a Pagar es mayor al consumo...");
 							return Mono.empty();
-						}else {
-							wCClient.put().uri("/updateBalanceAccounByAccountNumberPayment/{accountNumber}/{quantity}",params).retrieve().bodyToFlux(Client.class).subscribe();
-							
-							Transaction objT = new Transaction();
-							
-							objT.setDate(new Date());
-							objT.setAccountNumber(accNumber);
-							objT.setQuantity(quantity);
-							objT.setTransactionType("Pago");
-							objT.setOriginMov(originMov);
-							return service.save(objT);
 						}
+						
+						
+						
 					});
-			
-		}else if(typePay == true){ //Pago desde una Cuenta Bancaria::
-			log.info("Ingreso por que es nro Cuenta..");
-			//Validar si cuenta con saldo la Cuenta Bancaria.
-			
-			
-			Map<String,Object> paramsp = new HashMap();
-			paramsp.put("numAcc", numberAccountV);
-			
-			Account monoAcc= new Account();
-			monoAcc.setAccountstatus('N');
-			monoAcc.setBalance(0);
-					wCAccount.get().uri("/findAccountByNumberAccount/{numAcc}",paramsp)
-					.retrieve()
-					.bodyToMono(Account.class).switchIfEmpty(Mono.just(monoAcc))
-					.flatMap(objMAcc ->{
+					
+					
 						
-						if(objMAcc.getBalance() > 0  && quantity <= objMAcc.getBalance() ) {
-							log.info("Procede con el pago descuento en la cuenta Corriente. -> "+objMAcc.getBalance());	
-							
-							
-							Map<String,Object> paramsUpAcc = new HashMap();
-							paramsUpAcc.put("accountNumber", objMAcc.getAccountNumber());
-							paramsUpAcc.put("quantity", quantity);
-							
-							wCAccount.put().uri("/updateBalanceAccountRetireByAccountNumber/{accountNumber}/{quantity}",paramsUpAcc).retrieve().bodyToMono(Account.class).subscribe();
-							
-							wCClient.put().uri("/updateBalanceAccountRetireByAccountNumber/{accountNumber}/{quantity}",params).retrieve().bodyToFlux(Client.class).subscribe();
-							wCPerson.put().uri("/updateBalanceAccountRetireByAccountNumber/{accountNumber}/{quantity}",params).retrieve().bodyToFlux(Person.class).subscribe();
-							
-						}else if(quantity > objMAcc.getBalance()) {
-							log.warn("No cuenta con saldo suficiente para la cantidad a Pagar..");
-						}					
-						else{
-							log.warn("No cuenta con saldo..");
-						}
-						
-						log.info("Info sobre la consulta del MOno Cuenta. -> "+objMAcc.getBalance());	
-						return Mono.empty();
-					}).subscribe();
-			
-			
-			
-			
-			return Mono.empty();
-		}
-		return Mono.empty();
-		
-		
-		
-		
-		
-		
+
+					
+				});
+
 	}
 	
 	
-	// Consult los Movimientos de un CLiente(dni): mostrar transacciones de las cuentas que le pertenecen.
+	
+	
+
+	// Consult los Movimientos de un CLiente(dni): mostrar transacciones de las
+	// cuentas que le pertenecen.
 	@ApiOperation(value = "RQ11-Check movements of my products", notes = "Returns all movements on all accounts that a customer has by entering their document number")
 	@GetMapping("/findTransactionByNumberDocuCLient/{numberDocument}")
-	public Flux<Transaction> findTransactionByNumberDocuCLient(@PathVariable String numberDocument){
-		
-		Map<String,Object> params = new HashMap();
+	public Flux<Transaction> findTransactionByNumberDocuCLient(@PathVariable String numberDocument) {
+
+		Map<String, Object> params = new HashMap();
 		params.put("nroDoc", numberDocument);
-		
-		//Mono
-		return wCClient.get().uri("/BuscarClientePorNroDoc/{nroDoc}",params)
-				.retrieve()
-				.bodyToMono(Client.class)
-				
-				.map(objClient->{
-					
+
+		// Mono
+		return wCClient.get().uri("/BuscarClientePorNroDoc/{nroDoc}", params).retrieve().bodyToMono(Client.class)
+
+				.map(objClient -> {
+
 					List<Account> lstAccBank = objClient.getAccountList();
 					List<CreditAccount> lstAccCred = objClient.getCreditAccountList();
-					
+
 					for (CreditAccount accountC : lstAccCred) {
 						Account cred = new Account();
 						Integer nr = accountC.getAccountNumber();
 						cred.setAccountNumber(nr);
 						lstAccBank.add(cred);
 					}
-					
+
 					return lstAccBank;
-					
-				}).flatMapMany(lst -> Flux.fromIterable(lst))
-				.flatMap(objB->{
+
+				}).flatMapMany(lst -> Flux.fromIterable(lst)).flatMap(objB -> {
 					Integer nroAcc = objB.getAccountNumber();
-					
+
 					return service.getTranByNroAccount(nroAcc);
 				});
-		//return Flux.empty();
-		
+		// return Flux.empty();
+
 	}
-	
-	
+
 	@GetMapping("/prub/{nroAcc}")
-	public Flux<Transaction> prub(@PathVariable Integer nroAcc){
+	public Flux<Transaction> prub(@PathVariable Integer nroAcc) {
 		return service.getTranByNroAccount(nroAcc);
 	}
-	
-	
+
 }
